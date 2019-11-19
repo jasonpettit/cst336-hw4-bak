3e146194efdd1a0e5934846434f952915e87dcaf
EC2 Default User <ec2-user@ip-172-31-42-25.us-east-2.compute.internal>
2019-11-19 04:30:16 +0000

    complete v1.0

--- app.js
+++ app.js
@@ -5,30 +5,22 @@ app.use(express.static("public"));
 
 var faker = require('faker');
 var word1 = faker.hacker.adjective();
-var word2 = faker.hacker.noun();
-var word3 = faker.hacker.noun();
-var word4 = faker.hacker.verb();
-var word5 = faker.hacker.ingverb();
-var word6 = faker.hacker.noun();
-var word7 = faker.hacker.verb();
 
 //routes
 app.get ("/", function (req, res) {
-    res.render("index.ejs", {fakeword1:word1}, {fakeword2:word2}, 
-    {fakeword3:word3}, {fakeword4:word4}, {fakeword5:word5}, 
-    {fakeword6:word6}, {fakeword7:word7});
+    res.render("index.ejs", {fakeword1:word1});
 })
 
 app.get("/companies", function(req, res){
-    res.render("companies.html");
+    res.render("companies.ejs", {fakeword1:word1});
 });
 
 app.get("/future", function(req, res){
-    res.render("future.html");
+    res.render("future.ejs", {fakeword1:word1});
 });
 
 app.get("/how", function(req, res){
-    res.render("how.html");
+    res.render("how.ejs", {fakeword1:word1});
 });
 
 //CLOUD 9 CONFIG
